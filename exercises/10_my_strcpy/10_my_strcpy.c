318b30885730462c55d4f034852c847193fbcf4c
LiuZhangHao <1715139614@qq.com>
2026-04-22 04:32:06 +0800

    feat: 实现my_strcpy函数的基本功能

--- exercises/10_my_strcpy/10_my_strcpy.c
+++ exercises/10_my_strcpy/10_my_strcpy.c
@@ -4,9 +4,18 @@ int main() {
     char source[] = "Hello, World! This is a test string for my_strcpy function.\n";
     char destination[101];
     
-	// TODO: 在这里添加你的代码
-    // I AM NOT DONE
+	int p = 0 ;
+    char c = source[p];
+
+    do{
+        c = source[p];
+        destination[p] = c;
+        p++;
+    }while(c!='\0');
+
     
+
+
     printf("拷贝后的字符串: %s", destination);
     
     return 0;
